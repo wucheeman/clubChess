7711fcbcdfc0727847d5c8eaab9c8beaf206eebc
Mark Hainline <wucheeman@gmail.com>
2018-07-17 20:39:17 -0400

    corrected typo causing Heroku crash

--- routes/auth.js
+++ routes/auth.js
@@ -5,7 +5,7 @@ require('../config/passport')(passport);
 var express = require('express');
 var jwt = require('jsonwebtoken');
 var router = express.Router();
-var User = require("../models/user");
+var User = require("../models/Ugser");
 
 // register new user
 router.post('/register', function(req, res) {
