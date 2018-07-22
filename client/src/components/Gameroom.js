90575332485e1b25d65fba78498ffe8b5f149665
Mark Hainline <wucheeman@gmail.com>
2018-07-22 08:49:50 -0400

    updated gameroom to design from plan a

--- client/src/components/Gameroom.js
+++ client/src/components/Gameroom.js
@@ -4,12 +4,21 @@ import { Link } from 'react-router-dom';
 export default class Gameroom extends React.Component {
   render() {
     return (
-      <div>
-        <h1>Got here</h1>
-        <Link to="/game">
-        Game On!
-      </Link>
-    </div>
+      <div class="page lobby" id='page-lobby'>
+      <h1>Game Room</h1>
+        <h2 id='userLabel'></h2>
+        <h3>Active games</h3>
+        <div id='gamesList'>
+          No active games
+        </div>
+        <Link to="/game">Game On!</Link>
+        <h3>Online players</h3>
+        <div id='userList'>
+          No users online
+        </div>
+        <Link to="/">Back to Lobby</Link>
+      </div>
     );
   }
 }
+
