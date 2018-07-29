cc5e284e3fc3386df5a59f5444da0230e49c879c
Mark Hainline <wucheeman@gmail.com>
2018-07-29 08:37:29 -0400

    corrected background color on breadcrumbs

--- client/src/components/Lobby.js
+++ client/src/components/Lobby.js
@@ -4,6 +4,8 @@ import { Link } from 'react-router-dom';
 import axios from 'axios';
 import Navbar from './Navbar';
 import Wrapper from './Wrapper';
+import './Page.css';
+
 
 class Lobby extends Component {
 
