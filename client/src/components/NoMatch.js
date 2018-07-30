ba9f7b63652645617fad2641f80fc10e380d23e6
Mark Hainline <wucheeman@gmail.com>
2018-07-30 06:34:53 -0400

    enhancements

--- client/src/components/NoMatch.js
+++ client/src/components/NoMatch.js
@@ -5,14 +5,18 @@ const NoMatch = () => (
   <div className="row">
     <div className="col-sm-12 text-center">
           <br />
+          <br />
           <h1>404</h1>
+          <br />
           <h2>This is not the web page you were looking for...</h2>
+          <br />
           <h1>
-            <span role="img" aria-label="Face With Rolling Eyes Emoji">
-              🙄
+            <span role="img" aria-label="Confused Face Emoji">
+              😕
             </span>
-            <h2>but we don't know where that one is. Sorry!</h2>
           </h1>
+          <br />
+          <h2>but we don't know where that one is. Sorry!</h2>
       </div>
     </div>
   </div>
