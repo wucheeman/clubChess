642d59905f21207e9f1ee151df0514e1e5947c8c
Mark Hainline <wucheeman@gmail.com>
2018-07-31 16:27:22 -0400

    removed deadcode from index

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import { BrowserRouter as Router, Route } from 'react-router-dom';
-// import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-// import '../node_modules/bootstrap/dist/css/bootstrap-theme.min.css';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
